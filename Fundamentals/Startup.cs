5e0e34659e1b6aa93b3712cada6695298fd7b0fc
sumit7747 <sumit7747@gmail.com>
2018-03-12 14:24:33 +0530

    Add project files.

---
+++ Fundamentals/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Fundamentals.Startup))]
+namespace Fundamentals
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
